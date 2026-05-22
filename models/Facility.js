afc473074016935caaee22653ff0959e68a632fd
masudrana-mdra <mr3377006@gmail.com>
2026-05-22 11:23:35 +0600

    feat: define facility data model

---
+++ models/Facility.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const facilitySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    facility_type: { type: String, required: true },
+    image: {
+        type: String,
+        required: true,
+        match: [/^https?:\/\//i, 'Image URL must start with http:// or https://']
+    },
+    location: { type: String, required: true },
+    price_per_hour: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    available_slots: { type: [String], required: true },
+    description: { type: String, required: true },
+    owner_email: { type: String, required: true },
+    booking_count: { type: Number, default: 0 }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Facility', facilitySchema);
