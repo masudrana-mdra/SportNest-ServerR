0f67a08802178bde81b9d8ba52d53cc9bad41f02
masudrana-mdra <mr3377006@gmail.com>
2026-05-22 11:23:35 +0600

    feat: define user data model for persistence

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        // Optional because Google OAuth users won't have a password
+    },
+    photoURL: {
+        type: String,
+        default: ''
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'owner'],
+        default: 'user'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
